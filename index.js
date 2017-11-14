84cca27ec3e6ae904cf0b28e6ae82699297995a2
Chris Pang <chris_pang429@outlook.com>
2017-11-14 00:56:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,24 @@ class BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
 
+  BoardMember.prototype.veto = function() {
+    return "No, I must disagree";
+  }
 
+  BoardMember.prototype.approve = function() {
+    return "You can do that!";
+  }
+
+  BoardMember.prototype.doCharity = funtion() {
+    return "I like to help people.";
+  }
+
+  BoardMember.prototype.releasePressStatement() {
+    return "You will see great things from Scuber.";
+  }
+
+  BoardMember.prototype.sayHi = function() {
+    return `Hi, name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+  }
+    
 }
